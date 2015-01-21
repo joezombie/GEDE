506d0108b73b35678b9083eceb1c01dfb43e3a33
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2015-01-21 22:51:57 +0000

    lab2

--- LAB_02/main.cpp
+++ LAB_02/main.cpp
@@ -11,9 +11,15 @@ class MyFrameListener : public Ogre::FrameListener {
 private:
 	OIS::InputManager* _inputManager;
 	OIS::Keyboard* _keyboard;
+	OIS::Mouse* _mouse;
+	Ogre::Camera* _cam;
+	float _movementSpeed;
 
 public:
-	MyFrameListener(Ogre::RenderWindow* win){
+	MyFrameListener(Ogre::RenderWindow* win, Ogre::Camera* cam){
+		_cam = cam;
+		_movementSpeed = 50.0f;
+
 		OIS::ParamList parameters;
 		unsigned int windowHandle = 0;
 		std::ostringstream windowHandleString;
@@ -26,19 +32,49 @@ public:
 		_inputManager = OIS::InputManager::createInputSystem(parameters);
 
 		_keyboard = static_cast<OIS::Keyboard*>(_inputManager->createInputObject(OIS::OISKeyboard, false));
+
+		_mouse = static_cast<OIS::Mouse*>(_inputManager->createInputObject(OIS::OISMouse, false));
 	}
 
 	~MyFrameListener(){
 		_inputManager->destroyInputObject(_keyboard);
+		_inputManager->destroyInputObject(_mouse);
 		OIS::InputManager::destroyInputSystem(_inputManager);
 	}
 
 	bool frameStarted(const Ogre::FrameEvent& evt){
+		// Keyboard
 		_keyboard->capture();
 
+		// Stop on escape key
 		if (_keyboard->isKeyDown(OIS::KC_ESCAPE)){
 			return false;
 		}
+
+		// Move camera with WASD
+		Ogre::Vector3 translate(0, 0, 0);
+		if (_keyboard->isKeyDown(OIS::KC_W)){
+			translate += Ogre::Vector3(0, 0, -1);
+		}
+		if (_keyboard->isKeyDown(OIS::KC_S)){
+			translate += Ogre::Vector3(0, 0, 1);
+		}
+		if (_keyboard->isKeyDown(OIS::KC_A)){
+			translate += Ogre::Vector3(-1, 0, 0);
+		}
+		if (_keyboard->isKeyDown(OIS::KC_D)){
+			translate += Ogre::Vector3(1, 0, 0);
+		}
+		_cam->moveRelative(translate * evt.timeSinceLastFrame * _movementSpeed);
+
+		// Mouse
+		_mouse->capture();
+		// Move camera with mouse
+		float rotX = _mouse->getMouseState().X.rel * evt.timeSinceLastFrame * -1;
+		float rotY = _mouse->getMouseState().Y.rel * evt.timeSinceLastFrame * -1;
+		_cam->yaw(Ogre::Radian(rotX));
+		_cam->pitch(Ogre::Radian(rotY));
+
 
 		return true;
 	}
@@ -102,9 +138,6 @@ public:
 
 		Ogre::RenderWindow* window = _root->initialise(true, "GEDE LAB_02 JohannesH10@ru.is");
 
-		_listener = new MyFrameListener(window);
-		_root->addFrameListener(_listener);
-
 		_sceneManager = _root->createSceneManager(Ogre::ST_GENERIC);
 
 		Ogre::Camera* camera = _sceneManager->createCamera("Camera");
@@ -115,6 +148,9 @@ public:
 		Ogre::Viewport* viewport = window->addViewport(camera);
 		viewport->setBackgroundColour(Ogre::ColourValue(0.0, 0.0, 0.0));
 		camera->setAspectRatio(Ogre::Real(viewport->getActualWidth() / Ogre::Real(viewport->getActualHeight())));
+
+		_listener = new MyFrameListener(window, camera);
+		_root->addFrameListener(_listener);
 
 		loadResources();
 		createScene();
