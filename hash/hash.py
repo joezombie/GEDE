f1e91c10ab4e40792374e1ed641b8d83e71aed86
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2015-03-20 14:22:53 +0000

    Bleh

--- hash/hash.py
+++ hash/hash.py
@@ -5,24 +5,34 @@ def my_hash(s):
     i = 0
     res = 0
     for c in s:
-        if i == 0:
-            res = res ^ ord(c)
-        elif i == 1:
-            res = res ^ (ord(c) << 8)
-        elif i == 2:
-            res = res ^ (ord(c) << 16)
-        elif i == 3:
-            res = res ^ (ord(c) << 24)
-        elif i == 4:
+        if i < 32:
+            res = res ^ ((ord(c) % 15) << i)
+        else:
             i = 0
-        i += 1
+        i += 4
     return res
 
 
 words = ['VEHICLE1', 'VEHICLE2', 'AMOB', 'BMOB']
 
 for word in words:
-    murmur = mmh3.hash(word)
     my = my_hash(word)
-    print('Murmurhash {0} {1}'.format(murmur, murmur % 1024))
-    print('MyHash {0} {1}'.format(my, my % 1024))
+    print('HashFunction1("{0}") = {1}'.format(word, my))
+
+print('\n')
+
+for word in words:
+    murmur = mmh3.hash(word)
+    print('HashFunction2("{0}") = {1}'.format(word, murmur))
+
+print('\n')
+
+for word in words:
+    my = my_hash(word)
+    print('HashFunction1("{0}") % 1024 = {1}'.format(word, my % 1024))
+
+print('\n')
+
+for word in words:
+    murmur = mmh3.hash(word)
+    print('HashFunction2("{0}") % 1024 = {1}'.format(word, murmur % 1024))
